00a94884477e82aad0a3983b488412d59eeccbe0
Will Dietz <w@wdtz.org>
2016-05-12 17:35:25 -0500

    Remove category atom feed links, they're broken and not important.

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'http://wdtz.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
