2225edb9d87074c7ea8dd3f388a8b4c924d740f4
Will Dietz <w@wdtz.org>
2019-02-13 01:55:47 -0600

    branding

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,9 +52,9 @@ DEV_THEMES = [
 # Social widget
 SOCIAL = (
     ('Email', 'mailto:wdietz2@uiuc.edu', 'fa-envelope'),
-    ('Mastodon', 'https://mastodon.social/wdtz', 'fa-mastodon'),
-    ('Twitter', 'https://twitter.com/wdtz', 'fa-twitter'),
-    ('Github', 'https://github.com/dtzWill', 'fa-github'),
+    ('Mastodon', 'https://mastodon.social/wdtz', 'fab fa-mastodon'),
+    ('Twitter', 'https://twitter.com/wdtz', 'fab fa-twitter'),
+    ('Github', 'https://github.com/dtzWill', 'fab fa-github'),
     #('Facebook', 'https://facebook.com/dtzWill', 'fa-facebook-square'),
     #('Google-Plus', 'http://wdtz.org/+', 'fa-google-plus'),
     ('##uiuc on Freenode', 'https://webchat.freenode.net/?channels=%23%23uiuc', 'fa-comment'),)
